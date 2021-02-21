0e14630d43ba57099a216a1a084a84cf98816c69
deokjoo <oriho@daum.net>
2021-02-21 23:06:15 +0900

    ddd

--- t00/axis_agent/axis_agent_cfg.svh
+++ t00/axis_agent/axis_agent_cfg.svh
@@ -12,21 +12,7 @@ class axis_agent_cfg extends uvm_object:
         super.new(name)
     endfunction
 
-    extern virtual function void build_phase (uvm_phase phase);
-
 endclass
-
-//
-//
-virtual function void axis_agent_cfg::build_phase (uvm_phase phase);
-    super.build_phase(phase)
-
-    if( !uvm_config_db#(filter_env_cfg)::get(this, "", "cfg", m_env_cfg) )
-        `uvm_fatal(get_full_name(), "cannot get m_env_cfg")
-
-
-
-endfunction
 
 
 
