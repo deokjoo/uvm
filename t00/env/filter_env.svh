8a958bfedc4619a2973d7afefd062a7f1ef945e7
deokjoo <oriho@daum.net>
2021-02-24 22:29:00 +0900

    today.

--- t00/env/filter_env.svh
+++ t00/env/filter_env.svh
@@ -42,7 +42,8 @@ class filter00_env extends filter_env;
     axis_agent      m_axis_s00    ;
     axis_scbd       m_axis_scbd   ;
     
-    frame_seq       m_seqs        ;
+    filter_seq      m_seq         ;
+    filter_sqr      m_sqr         ;
 
     function new(string name="filter00_env", uvm_component parent=null);
         super.new(name, parent);
@@ -65,10 +66,8 @@ function void filter00_env::build_phase(uvm_phase phase);
     m_axis_s00     = axis_agent    ::type_id::create("m_axis_s00" ,    this);
    
     m_axis_scbd    = axis_scbd     ::type_id::create("m_axis_scbd",    this);
-    m_seqs         = frame_seq     ::type_id::create("m_seqs",         this);
-
-    //set frame seq
-    m_seqs.seqs = 3;
+    m_seq          = filter_seq    ::type_id::create("m_seq",          this);
+    m_sqr          = filter_sqr    ::type_id::create("m_sqr",          this);
 
     //set config m00
     m_axis_m00_cfg._if = m_env_cfg.axis_if_m00;
@@ -91,6 +90,10 @@ function void filter00_env::connect_phase(uvm_phase phase);
 
     m_axis_m00.m_mon.m_analysys_port.connect(m_axis_scbd.m_analysis_imp);
 
+    // connect sequencer to virtual sequencer
+    m_sqr.m_axis_seqr = m_axis_m00.m_sqr;
+
+
 endfunction
 
 //
@@ -100,7 +103,7 @@ task filter00_env::run_phase(uvm_phase phase);
 
     phase.raise_objection(this);
 
-    m_seqs.start(m_axis_m00.m_sqr);
+    m_seq.start(m_sqr);
 
     phase.drop_objection(this);
 
