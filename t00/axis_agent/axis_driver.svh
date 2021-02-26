6421cb23fc647e1313fe9890c8428be4ffb6dcd5
deokjoo <oriho@daum.net>
2021-02-26 23:38:31 +0900

    add drvier

--- t00/axis_agent/axis_driver.svh
+++ t00/axis_agent/axis_driver.svh
@@ -3,10 +3,47 @@
 //
 //
 //---------------------------------------------------------
+class frame_reader extends uvm_object;
+    `uvm_object_utils(frame_reader)
 
+    int            m_fd  ;
+
+    function new(string name="frame_reader");
+        super.new(name);
+    endfunction
+
+    //
+    function int open(frame_seq_item item);
+        `uvm_info("DRV", $sformatf("open file: %0d %s", $time, item.m_input_file), UVM_LOW);        
+        m_fd = $fopen(item.m_input_file,"r");
+        if(!m_fd) begin
+            `uvm_fatal(get_full_name(), "file open failed")
+            return 0;
+        end
+
+        return 1;
+
+    endfunction
+
+    function int close();
+        $fclose(m_fd);
+    endfunction
+
+    function int read();
+        int pixel;
+        assert($fscanf(m_fd, "%h", pixel) == 1);        
+        return pixel;
+    endfunction 
+
+endclass
+
+//---------------------------------------------------------
+//
+//
+//
+//---------------------------------------------------------
 class axis_driver extends uvm_driver #(frame_seq_item);
-    `uvm_component_utils(axis_driver);
-        
+    `uvm_component_utils(axis_driver);        
     axis_agent_cfg m_cfg;
     virtual axis_if _if ;
 
@@ -17,7 +54,7 @@ class axis_driver extends uvm_driver #(frame_seq_item);
     //
     extern virtual function void build_phase(uvm_phase phase);
     extern virtual task run_phase(uvm_phase phase);
-    extern virtual task drive_item(frame_seq_item item);
+    extern virtual task drive_item(frame_reader reader);
 
 endclass
 
@@ -36,37 +73,51 @@ endfunction
 //
 //
 task axis_driver::run_phase(uvm_phase phase);
-    frame_seq_item data;
-        
-    super.run_phase(phase);
-    
+    frame_seq_item item ;
+    frame_reader reader = new();        
+    super.run_phase(phase);    
     forever begin
-        seq_item_port.get_next_item(data);
-        drive_item(data);
+        seq_item_port.get_next_item(item);
+
+        reader.open(item);
+        drive_item(reader);
+        reader.close();        
         seq_item_port.item_done();
     end
-
 
 endtask
 
 //
-task axis_driver::drive_item(frame_seq_item item);
+//
+//
+task axis_driver::drive_item(frame_reader reader);
+    int pixel;
 
-    repeat(10) @(posedge _if.aclk);
-    `uvm_info("DRV", $sformatf("drive item : %0d %s", $time, item.convert2str()), UVM_LOW);
-
-    @(_if.cb_master)
-    _if.cb_master.axis_tvalid<= 1;
-    
-    for(int i=0; i<3; i++) begin
+    repeat(10) @(_if.cb_master);    
+    for(int i=0; i<3; i++) begin     
         for(int j=0; j<3; j++) begin
-            @(_if.cb_master)
-            _if.cb_master.axis_tdata <= j;
+            // control signal
+            _if.cb_master.axis_tvalid<= 1;            
+            if(i==0 && j==0)
+                _if.cb_master.axis_tuser <= 1;
+            else
+                _if.cb_master.axis_tuser <= 0;               
+            if(j==2)
+                _if.cb_master.axis_tlast <= 1;
+            else
+                _if.cb_master.axis_tlast <= 0;                                     
+            //data    
+            pixel = reader.read();
+            _if.cb_master.axis_tdata <= pixel;               
+            @(_if.cb_master);
         end
+
+        _if.cb_master.axis_tvalid<= 0;
+        _if.cb_master.axis_tlast <= 0;
+        @(_if.cb_master);
     end
 
     @(_if.cb_master)
-    _if.cb_master.axis_tvalid<= 0;
 
     //
     repeat(10) @(_if.cb_master);
